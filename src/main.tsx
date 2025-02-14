d06702551adaed2ecde22ecea9ee3d07759e1976
Biniam batu <Biniambatu12@gmail.com>
2025-02-14 17:54:29 +0300

    latest collection page

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter as Router } from 'react-router-dom'
+import ShopContextProvider from './Context/ShopContext.tsx'
 
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router>
-       <App />
-    </Router>
+    <ShopContextProvider>
+      <Router>
+        <App />
+      </Router>
+    </ShopContextProvider>
   </StrictMode>,
 )
